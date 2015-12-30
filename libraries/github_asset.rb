018e4aab3e459fd5a375d52efa123c9c1608a613
Lance Hudson <lance.hudson@hightail.com>
2015-12-30 03:49:36 -0500

    remove a debug message and force remove file on checksum error

--- libraries/github_asset.rb
+++ libraries/github_asset.rb
@@ -60,7 +60,6 @@ module GithubCB
 
     # @param [String] filepath
     def file_checksum filepath
-      puts filepath
       Digest::SHA256.file(filepath.to_s).hexdigest
     end
 
@@ -125,6 +124,7 @@ module GithubCB
         end
       end
     rescue GithubCB::ChecksumMismatch => ex
+      FileUtils.rm_rf(options[:path])
       puts "Failed Checksum, Retries left #{options[:retries]}"
       if options[:retries] <= 0
         fail ex
