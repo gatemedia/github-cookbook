992f4e461964373e0b42f7919aea6d2eeb1533a0
Pulkit Goyal <pulkit110@gmail.com>
2024-03-19 08:27:27 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name              'github'
-maintainer        'Sous Chefs'
-maintainer_email  'help@sous-chefs.org'
+maintainer        'GateMedia'
+maintainer_email  'it@gatemedia.ch'
 license           'Apache-2.0'
 description       'A Library Cookbook for interacting with the Github API'
-version           '1.1.10'
-source_url        'https://github.com/sous-chefs/github'
-issues_url        'https://github.com/sous-chefs/github/issues'
+version           '2.0.0'
+source_url        'https://github.com/gatemedia/github-cookbook'
+issues_url        'https://github.com/gatemedia/github-cookbook/issues'
 chef_version      '>= 15.3'
 
 supports "ubuntu"
